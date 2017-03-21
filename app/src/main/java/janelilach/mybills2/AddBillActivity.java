31722abca0dfd2da7e38b9b4fba1bc26e0486e49
lil_b <likewavescrashing@gmail.com>
2017-03-20 21:28:50 -0400

    fixed adding invalid bill, swapped date field for datePicker

--- app/src/main/java/janelilach/mybills2/AddBillActivity.java
+++ app/src/main/java/janelilach/mybills2/AddBillActivity.java
@@ -1,27 +1,34 @@
 package janelilach.mybills2;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AddBillActivity extends AppCompatActivity {
 
     private boolean paid;
-    private boolean recurring;
     private Intent toSend;
     private Date oldPaidDate;
+    private Date dueDate = new Date();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +49,7 @@ public class AddBillActivity extends AppCompatActivity {
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner_types);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this,R.layout.spinner_item_type,arrayList1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item_type, arrayList1);
         adapter.setDropDownViewResource(R.layout.spinner_dropdown);
         adapter.notifyDataSetChanged();
         spinner.setAdapter(adapter);
@@ -54,6 +61,7 @@ public class AddBillActivity extends AppCompatActivity {
         setSupportActionBar(actionBar);
         getSupportActionBar().setTitle("HOME > ADD BILL");
 
+        // get old bill info, and set fields
         toSend = getIntent();
         Bundle receivedB = toSend.getBundleExtra("bill");
 
@@ -69,92 +77,102 @@ public class AddBillActivity extends AppCompatActivity {
             //ArrayAdapter adapter = (ArrayAdapter) spinner.getAdapter();
             spinner.setSelection(adapter.getPosition(oldBill.type));
 
-            SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
-            ((EditText) findViewById(R.id.add_bill_date)).setText(df.format((oldBill.dueDate)));
+            dueDate = oldBill.dueDate;
+
 
             if (oldBill.paidDate != null) {
                 ((RadioButton) findViewById(R.id.add_bill_status_paid)).setChecked(true);
                 oldPaidDate = new Date(oldBill.paidDate.getTime());
+                paid = true;
             } else {
                 ((RadioButton) findViewById(R.id.add_bill_status_unpaid)).setChecked(true);
                 oldPaidDate = null;
             }
+        } else {
+            ((RadioButton) findViewById(R.id.add_bill_status_unpaid)).setChecked(true);
         }
 
+        // init datePicker to dueDate (today if new bill, or old due date if old bill)
+        DatePicker datePicker = (DatePicker) findViewById(R.id.datePicker);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(dueDate);
+        datePicker.init(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
+
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                Calendar cal = Calendar.getInstance();
+                cal.set(year, month, dayOfMonth);
+                dueDate = cal.getTime();
+
+            }
+        });
 
     }
 
     public void onAddBillConfirmClick(View view) {
 
+        Boolean invalid = false;
         String color;
-        Date dueDate;
 
-        SimpleDateFormat df = new SimpleDateFormat("MM-dd-yyyy");
+//        SimpleDateFormat df = new SimpleDateFormat("MM-dd-yyyy");
 
-
+        // parse name
         String name = ((EditText) findViewById(R.id.add_bill_name)).getText().toString();
+        if (name.equals("")) {
+            invalid = true;
+        }
 
-        Double amount = Double.parseDouble(((EditText) findViewById(R.id.add_bill_amount)).getText().toString());
+        // parse amount
+        String amountString = ((EditText) findViewById(R.id.add_bill_amount)).getText().toString();
+        String pattern = "^\\d*\\.\\d{2}$";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(amountString);
+        if (!m.matches()) {
+            invalid = true;
+        }
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner_types);
 
         String type = spinner.getSelectedItem().toString();
 
+
+
         Date paidDate = null;
         Date today = new Date();
 
-        String date = ((EditText) findViewById(R.id.add_bill_date)).getText().toString();
-        Log.v("dateToday", date);
-
-        try {
-            dueDate = df.parse(date);
-            Log.v("dateDue", dueDate.toString());
-
-            if (oldPaidDate != null) {
-                paid = true;
+        // may need to fix the logic of setting paid dates here
+        if (paid) {
+            color = "green";
+            Log.v("bill color", color);
+            if (oldPaidDate == null) { // case 1: adding new paid bill
+                paidDate = today;
+            } else { // case 2: editing old paid bill
+                paidDate = oldPaidDate;
             }
-
-//            Log.v("addBill", oldPaidDate.toString());
-
-            // may need to fix the logic of setting paid dates here
-            if (paid) {
-                color = "green";
-                Log.v("bill color",color);
-                if (oldPaidDate == null) {
-                    paidDate = new Date();
-                } else {
-                    paidDate = oldPaidDate;
-                }
-                if (paidDate.after(dueDate)) { // case 1: editing bill status to paid
-
-                    String paidDateString = df.format(today);
-                    paidDate = df.parse(paidDateString);
-                } else { // case 2: adding new bill that was paid (assume paid same date)
-                    paidDate = dueDate;
-                }
-            } else {
-                if (dueDate.after(today)) {  // case 3: adding unpaid bill due in the future
-                    color = "yellow";
-                } else { // case 4: adding unpaid bill due today or earlier
-                    color = "red";
-                }
+            if (paidDate.after(dueDate)) { // case 3: editing old unpaid bill status to paid
+                paidDate = today;
             }
+        } else {
+            if (!dueDate.before(today)) {  // case 4: adding unpaid bill due in the future
+                color = "yellow";
+            } else { // case 5: adding unpaid bill due today or earlier
+                color = "red";
+            }
+        }
 
-            Log.v("bill color 2",color);
+        Log.v("bill color 2", color);
 
+        if (!invalid) {
             toSend = new Intent();
             Bundle b = new Bundle();
-            b.putSerializable("bill", new Bill(name, amount, dueDate, paidDate, type, recurring, color));
+            Double amount = Double.parseDouble(amountString);
+            b.putSerializable("bill", new Bill(name, amount, dueDate, paidDate, type, false, color));
             toSend.putExtra("bill", b);
 
-//            if (oldBill != null) {
-//                intent.putExtra("isEdit", true);
-//            }
             setResult(RESULT_OK, toSend);
             finish();
-        } catch (java.text.ParseException e) {
-            String stackTrace = e.getStackTrace().toString();
-            Log.v("Date creation error", stackTrace);
+        } else {
+            warnInvalidBill();
         }
 
     }
@@ -164,6 +182,10 @@ public class AddBillActivity extends AppCompatActivity {
 
         setResult(RESULT_CANCELED, toSend);
         finish();
+    }
+
+    public void warnInvalidBill() {
+        Toast.makeText(this, "TRYING TO ADD INVALID BILL", Toast.LENGTH_SHORT).show();
     }
 
     public void onRadioButtonClicked(View view) {
@@ -180,4 +202,6 @@ public class AddBillActivity extends AppCompatActivity {
                 break;
         }
     }
+
+
 }
